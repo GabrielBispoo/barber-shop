0d7985301e09ae9fc0e319a6814c99c7a8ce3bb0
GabrielBispoo <gabrielbispo20k@gmail.com>
2024-10-29 12:27:49 -0300

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+
+import NavBar from './components/NavBar.vue';
+import FooterBar from './components/FooterBar.vue';
+
+</script>
+
+<template>
+  <NavBar />
+  <router-view />
+  <FooterBar />
+</template>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+
+.font-YoungSerif{
+  font-family: 'YoungSerif';
+}
+
+.font-Hubballi{
+  font-family: 'Hubballi';
+}
+
+.font-HindVadodara{
+  font-family: 'HindVadodara';
+}
+
+img,
+video {
+  width: -webkit-fill-available;
+  height: inherit !important;
+  object-fit: cover;
+}
+
+body {
+  background-color: #000;
+}
+</style>
---
+++ src/components/FooterBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="nav__top">
+        <div class="logo__baber-shop">
+            <img src="" alt="">
+        </div>
+
+        <div class="nav__links">
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Promoções</li>
+                    <li>Serviços</li>
+                    <li>Equipe</li>
+                    <li>Destaques</li>
+                    <li>Agendamentos</li>
+                    <li>Loja</li>
+                </ul>
+            </nav>
+        </div>
+
+        <div class="mobile__menu">
+            <span>|||</span>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
